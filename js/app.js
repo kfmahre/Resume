a844697109ee9e8ae1fc05813f9678178fee5f54
kfmahre <kfmahre@gmail.com>
2017-12-10 20:47:19 -0500

    updated to bootstrap 4, ditched slick

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p class="white-text"><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry center-content"></div>';
+var HTMLprojectStart = '<div class="project-entry carousel-item center-content"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p class="white-text"><br>%data%</p>';
